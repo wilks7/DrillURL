f273c32dcd244447b3663407dfe586de918b2620
wilks7 <wilks7@icloud.com>
2023-08-10 16:20:51 -0400

    removed OSLog

--- Sources/DrillURL/DrillURL.swift
+++ Sources/DrillURL/DrillURL.swift
@@ -1,24 +1,36 @@
 
 import Foundation
 //import os.log
-import OSLog
+//import OSLog
 
 public protocol DrillClient {
     var baseURL: String {get}
     func makeRequest(for url: URL) -> URLRequest
     
     var session: URLSession {get}
-    var network_logger: Logger { get }
+//    var network_logger: Logger { get }
     var log_level: [LogLevel] {get}
 
 }
 
+public class PrintLogger {
+    func debug(_ string: String) {
+        Swift.print(string)
+    }
+    
+    func error(_ string: String) {
+        Swift.print(string)
+    }
+}
+
 public extension DrillClient {
+        
     var log_level: [LogLevel] { [.error, .request, .response] }
     var session: URLSession { URLSession.shared }
 
-    var network_logger: Logger {
-        Logger(subsystem: "package.DrillURL.Client", category: "Networking")
+    var network_logger: PrintLogger {
+        PrintLogger()
+//        Logger(subsystem: "package.DrillURL.Client", category: "Networking")
     }
-    var logger: Logger { network_logger }
+    var logger: PrintLogger { network_logger }
 }
